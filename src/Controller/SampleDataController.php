70360b216555960f1190e7a4c290f9c4e4b4a1b5
Carranya <k.gong82@gmail.com>
2023-01-26 22:11:38 +0100

    fixed bugs

--- src/Controller/SampleDataController.php
+++ src/Controller/SampleDataController.php
@@ -16,8 +16,8 @@ class SampleDataController extends AbstractController
     {
 
         $blogs = [
-            ['title' => 'Happy New Year 2023!!!', 'date' => '2023-01-22', 'descripe' => 'Wir wünschen Ihnen alles Gute ins Jahr des Hasens 2023!!!', 'img' => 'newyear'],
-            ['title' => 'Mondfest 2023', 'date' => '2022-09-10', 'descripe' => 'Alles Gute zum Mondfest 2022!!!', 'img' => 'moon'],
+            ['title' => 'Happy New Year 2023!!!', 'date' => '2023-01-22', 'descripe' => 'Wir wünschen Ihnen alles Gute ins Jahr des Hasens 2023!!!', 'img' => 'img/blog/newyear.jpg'],
+            ['title' => 'Mondfest 2023', 'date' => '2022-09-10', 'descripe' => 'Alles Gute zum Mondfest 2022!!!', 'img' => 'img/blog/moon.jpg'],
         ];
 
         foreach($blogs as $blog){
@@ -33,11 +33,11 @@ class SampleDataController extends AbstractController
         }
 
         $menues = [
-            ['name' => 'Frühlingsrollen', 'descripe' => 'knackige Frühlingsrollen mit vegetarischer Füllung', 'price' => '8.00', 'img' => 'springroll'],
-            ['name' => 'Fried Rice mit Cervetten', 'descripe' => 'Gebratene Reis nach Hongkong Art mit frischen Crevetten', 'price' => '23.00', 'img' => 'friedrice'],
-            ['name' => 'Szechuan', 'descripe' => 'Gericht nach Szechuan Art mit Poulet (scharf)', 'price' => '25.00', 'img' => 'szechuan'],
-            ['name' => 'Reis in Süss-Sauer Sauce', 'descripe' => 'Gedämpfte Reis in Süss-Sauer Sauce mit Poulet', 'price' => '18.00', 'img' => 'sweetsour'],
-            ['name' => 'Kungpao', 'descripe' => 'Reisgericht nach Kungpao Art', 'price' => '25.00', 'img' => 'kungpao'],
+            ['name' => 'Frühlingsrollen', 'descripe' => 'knackige Frühlingsrollen mit vegetarischer Füllung', 'price' => '8.00', 'img' => 'img/menu/springroll.jpg'],
+            ['name' => 'Fried Rice mit Cervetten', 'descripe' => 'Gebratene Reis nach Hongkong Art mit frischen Crevetten', 'price' => '23.00', 'img' => 'img/menu/friedrice.jpg'],
+            ['name' => 'Szechuan', 'descripe' => 'Gericht nach Szechuan Art mit Poulet (scharf)', 'price' => '25.00', 'img' => 'img/menu/szechuan.jpg'],
+            ['name' => 'Reis in Süss-Sauer Sauce', 'descripe' => 'Gedämpfte Reis in Süss-Sauer Sauce mit Poulet', 'price' => '18.00', 'img' => 'img/menu/sweetsour.jpg'],
+            ['name' => 'Kungpao', 'descripe' => 'Reisgericht nach Kungpao Art', 'price' => '25.00', 'img' => 'img/menu/kungpao.jpg'],
         ];
 
         foreach($menues as $menu){
